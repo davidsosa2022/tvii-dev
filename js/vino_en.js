61d6bd2010cb102150f345c7bdd0f5f6d041f499
davey stone but bfdi mouth <118952873+davidsosa2022@users.noreply.github.com>
2024-07-15 16:00:04 -0500

    2423

--- js/vino_en.js
+++ js/vino_en.js
@@ -1291,7 +1291,7 @@ tvii.router.connect("^/$", function () {
         }
         var prev = $(".live-program.selected").prev();
 
-        if (prev.length > 0 || !prev.hasClass("daily-tip")) {
+        if (prev.length > 0 && !prev.hasClass("daily-tip")) {
             clearInterval(tvii.scrollProgramListInterval)
             vino.soundPlayVolume("SE_PROGRAM_SLIDE_SPEED", 25);
             $(".live-program").removeClass("selected");
