75fa7cdf8a7c43d919705089c445a2060ccec327
davidsosa2022 <dwyazzo2000@gmail.com>
2024-07-20 18:45:46 -0500

    1231231

--- server/menu.php
+++ server/menu.php
@@ -42,37 +42,37 @@ if(!isset($_SERVER['HTTP_X_PJAX'])) {
           Select the TV/Film rating you want to set.
         </p>
         <div class="age-ratings program">
-          <label navi_target navi_no_reset navi_mouse class="age selected">
+          <label navi_target navi_no_reset navi_mouse class="age tv-y selected">
             <input type="radio" name="pc_age" value="TV-Y">
           </label>
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age tv-g">
             <input type="radio" name="pc_age" value="TV-G">
           </label>
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age tv-y7">
             <input type="radio" name="pc_age" value="TV-Y7">
           </label>
-          <label navi_target  navi_no_reset navi_mouse class="age">
+          <label navi_target  navi_no_reset navi_mouse class="age tv-pg">
             <input type="radio" name="pc_age" value="TV-PG">
           </label>
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age tv-14">
             <input type="radio" name="pc_age" value="TV-14">
           </label>
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age tv-14">
             <input type="radio" name="pc_age" value="TV-MA">
           </label>
         </div>
 
         <div class="age-ratings film">
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age g">
             <input type="radio" name="pc_age_film" value="G">
           </label>
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age pg">
             <input type="radio" name="pc_age_film" value="PG">
           </label>
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age pg13">
             <input type="radio" name="pc_age_film" value="PG-13">
           </label>
-          <label navi_target navi_no_reset navi_mouse class="age">
+          <label navi_target navi_no_reset navi_mouse class="age r">
             <input type="radio" name="pc_age_film" value="R">
           </label>
         </div>
