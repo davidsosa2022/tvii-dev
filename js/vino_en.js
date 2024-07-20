444366dd540c3c7476127bc650208d78de205da3
davidsosa2022 <dwyazzo2000@gmail.com>
2024-07-20 12:28:49 -0500

    34

--- js/vino_en.js
+++ js/vino_en.js
@@ -1601,12 +1601,12 @@ tvii.router.connect("^/program$", function () {
 
     prepareMiiverseModal();
 
-    var scrollStartFirstAmount = 439;
+var scrollStartFirstAmount = 439;
 var scrollStartSecondAmount = 465;
 var scrollBackFirstAmount = 460; // First scroll back amount
 var scrollBackSecondAmount = 760; // Second scroll back amount
 var intermediateScrollBackAmount = 465; // Intermediate scroll back amount
-var endScrollBackAmount = 365; // Scroll back amount from the end
+var endScrollBackAmount = 367; // Scroll back amount from the end
 var fadeDuration = 100;
 var isAnimating = false;
 
