43b5e197821829cb81969fb2dcfa6e3167cc98cd
davidsosa2022 <dwyazzo2000@gmail.com>
2024-07-20 01:40:20 -0500

    2342

--- js/vino_en.js
+++ js/vino_en.js
@@ -1603,8 +1603,8 @@ tvii.router.connect("^/program$", function () {
 
     var initialScrollAmount = 430;
     var subsequentScrollAmount = 470;
-    var scrollBackFirstAmount = 450; // Define the first scroll back offset
-    var scrollBackSecondAmount = 420; // Define the second scroll back offset
+    var scrollBackFirstAmount = 420; // Define the first scroll back offset
+    var scrollBackSecondAmount = 450; // Define the second scroll back offset
     var fadeDuration = 100;
     var isAnimating = false;
     
