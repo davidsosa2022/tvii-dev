5737683c64e9ccedbf17c909cdf0532d5718e88d
davey stone but bfdi mouth <118952873+davidsosa2022@users.noreply.github.com>
2024-07-28 18:15:42 -0500

    234

--- server/tabs-popular.php
+++ server/tabs-popular.php
@@ -7,6 +7,22 @@ if(!isset($_SERVER['HTTP_X_PJAX'])) {
 ?>
     <menu class="menu-buttons">
 
+    <div class="menu-modal-container none">
+      <div class="menu-tab">
+        <div class="search-section">
+        <span class="title">Menu</span>
+        <input type="text" navi_mouse="" navi_no_reset="" navi_target="" class="search_input" placeholder="Search for programs or actors..." vino_suggest="">
+        <a href="javascript:void(0)" navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" navi_target="" class="search-options" data-sound="SE_A_DECIDE_TOUCH_OFF">Search Options</a>
+      </div>
+      <div class="buttons-section">
+        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0)" navi_target="" class="favorites_settings" data-sound="SE_A_DECIDE_TOUCH_OFF">Favorites Settings</a>
+        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0);" navi_target="" class="electronic_manual" data-sound="SE_A_DECIDE_TOUCH_OFF">Electronic Manual</a>
+        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0);" navi_target="" class="app_settings" data-sound="SE_A_DECIDE_TOUCH_OFF">App Settings</a>
+        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0);" navi_target="" class="back_button accesskey-b" data-sound="SE_CLOSE_TOUCH_OFF"> Back</a>
+        </div>
+      </div>
+    </div>
+
       <header class="top-bar <?php if (!isset($_SERVER['HTTP_X_PJAX'])) echo 'loading-el'; ?>" data-page-first-load="0">
         <a href="javascript:void(0)" class="label popular selected" no_touch
         ><?php echo localize("vino.program.label.popular") ?></a>
@@ -212,22 +228,6 @@ if ($result) {
 // Close connection
 $conn->close();
 ?>
-    </div>
-
-    <div class="menu-modal-container none">
-      <div class="menu-tab">
-        <div class="search-section">
-        <span class="title">Menu</span>
-        <input type="text" navi_mouse="" navi_no_reset="" navi_target="" class="search_input" placeholder="Search for programs or actors..." vino_suggest="">
-        <a href="javascript:void(0)" navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" navi_target="" class="search-options" data-sound="SE_A_DECIDE_TOUCH_OFF">Search Options</a>
-      </div>
-      <div class="buttons-section">
-        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0)" navi_target="" class="favorites_settings" data-sound="SE_A_DECIDE_TOUCH_OFF">Favorites Settings</a>
-        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0);" navi_target="" class="electronic_manual" data-sound="SE_A_DECIDE_TOUCH_OFF">Electronic Manual</a>
-        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0);" navi_target="" class="app_settings" data-sound="SE_A_DECIDE_TOUCH_OFF">App Settings</a>
-        <a navi_mouse="" data-hover="" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" href="javascript:void(0);" navi_target="" class="back_button accesskey-b" data-sound="SE_CLOSE_TOUCH_OFF"> Back</a>
-        </div>
-      </div>
     </div>
 
 <?php
