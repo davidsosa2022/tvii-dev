2411ea065b7b493bc060b3f685f1009ae052f421
davey stone but bfdi mouth <118952873+davidsosa2022@users.noreply.github.com>
2024-07-31 22:18:44 -0500

    345

--- server/tabs-guide.php
+++ server/tabs-guide.php
@@ -41,268 +41,52 @@ if(!isset($_SERVER['HTTP_X_PJAX'])) {
     <div class="program-guide-settings none">
         <span class="title"><?php echo localize("vino.guide.settings.title") ?></span>
         <span class="title-label"><?php echo localize("vino.guide.settings.title.label") ?></span>
-        <div class="day-list">
+    <div class="day-list">
 
-        <div class="day-container">
-            <span class="label-day"></span>
-            <span class="day sun">4
-                <span class="name"><?php echo localize("vino.guide.day.sunday.short") ?></span>
-            </span>
+    <?php
+// Get the current day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+$currentDayIndex = date('w');
 
-            <a href="javascript:void(0)" navi_target navi_no_reset class="day-select selected">
-            <span>12:00AM</span>
-            <select name="day0" id="day0">
-                <option selected value="00:00">12:00AM</option>
-                <option value="01:00">1:00AM</option>
-                <option value="02:00">2:00AM</option>
-                <option value="03:00">3:00AM</option>
-                <option value="04:00">4:00AM</option>
-                <option value="05:00">5:00AM</option>
-                <option value="06:00">6:00AM</option>
-                <option value="07:00">7:00AM</option>
-                <option value="08:00">8:00AM</option>
-                <option value="09:00">9:00AM</option>
-                <option value="10:00">10:00AM</option>
-                <option value="11:00">11:00AM</option>
-                <option value="12:00">12:00PM</option>
-                <option value="13:00">1:00PM</option>
-                <option value="14:00">2:00PM</option>
-                <option value="15:00">3:00PM</option>
-                <option value="16:00">4:00PM</option>
-                <option value="17:00">5:00PM</option>
-                <option value="18:00">6:00PM</option>
-                <option value="19:00">7:00PM</option>
-                <option value="20:00">8:00PM</option>
-                <option value="21:00">9:00PM</option>
-                <option value="22:00">10:00PM</option>
-                <option value="23:00">11:00PM</option>
-            </select>
-            </a>
-        </div>
-        
-        <div class="day-container">
-            <span class="label-day"></span>
-            <span class="day mon">29
-                <span class="name"><?php echo localize("vino.guide.day.monday.short") ?></span>
-            </span>
+// Define the days of the week
+$daysOfWeek = [
+    'sun' => 'Sunday',
+    'mon' => 'Monday',
+    'tue' => 'Tuesday',
+    'wed' => 'Wednesday',
+    'thu' => 'Thursday',
+    'fri' => 'Friday',
+    'sat' => 'Saturday',
+];
 
-            <a href="javascript:void(0)" navi_target navi_no_reset class="day-select">
-            <span>12:00AM</span>
-            <select name="day1" id="day1">
-                <option selected value="00:00">12:00AM</option>
-                <option value="01:00">1:00AM</option>
-                <option value="02:00">2:00AM</option>
-                <option value="03:00">3:00AM</option>
-                <option value="04:00">4:00AM</option>
-                <option value="05:00">5:00AM</option>
-                <option value="06:00">6:00AM</option>
-                <option value="07:00">7:00AM</option>
-                <option value="08:00">8:00AM</option>
-                <option value="09:00">9:00AM</option>
-                <option value="10:00">10:00AM</option>
-                <option value="11:00">11:00AM</option>
-                <option value="12:00">12:00PM</option>
-                <option value="13:00">1:00PM</option>
-                <option value="14:00">2:00PM</option>
-                <option value="15:00">3:00PM</option>
-                <option value="16:00">4:00PM</option>
-                <option value="17:00">5:00PM</option>
-                <option value="18:00">6:00PM</option>
-                <option value="19:00">7:00PM</option>
-                <option value="20:00">8:00PM</option>
-                <option value="21:00">9:00PM</option>
-                <option value="22:00">10:00PM</option>
-                <option value="23:00">11:00PM</option>
-            </select>
-            </a>
-        </div>
+// Generate the day containers
+for ($i = 0; $i < 7; $i++) {
+    $dayIndex = ($currentDayIndex + $i) % 7;
+    $dayName = array_keys($daysOfWeek)[$dayIndex];
+    $dayNumber = ($i + 1) % 31; // Example: day number for display, adjust if needed
+    $dayLabel = array_values($daysOfWeek)[$dayIndex];
+    
+    echo '<div class="day-container">';
+    echo '    <span class="label-day"></span>';
+    echo '    <span class="day ' . $dayName . '">' . $dayNumber;
+    echo '        <span class="name"><?php echo localize("vino.guide.day.' . $dayName . '.short") ?></span>';
+    echo '    </span>';
+    echo '    <a href="javascript:void(0)" navi_target navi_no_reset class="day-select">';
+    echo '        <span>12:00AM</span>';
+    echo '        <select name="day' . $i . '" id="day' . $i . '">';
+    for ($hour = 0; $hour < 24; $hour++) {
+        $hourFormatted = str_pad($hour, 2, '0', STR_PAD_LEFT) . ':00';
+        $amPm = ($hour < 12) ? 'AM' : 'PM';
+        $hour12 = ($hour % 12) ?: 12;
+        echo '        <option value="' . $hourFormatted . '">' . $hour12 . ':00' . $amPm . '</option>';
+    }
+    echo '        </select>';
+    echo '    </a>';
+    echo '</div>';
+}
+?>
 
-        <div class="day-container">
-            <span class="label-day"></span>
-            <span class="day tue">30
-                <span class="name"><?php echo localize("vino.guide.day.tuesday.short") ?></span>
-            </span>
 
-            <a href="javascript:void(0)" navi_target navi_no_reset class="day-select">
-                <span>12:00AM</span>
-                <select name="day2" id="day2">
-                <option selected value="00:00">12:00AM</option>
-                <option value="01:00">1:00AM</option>
-                <option value="02:00">2:00AM</option>
-                <option value="03:00">3:00AM</option>
-                <option value="04:00">4:00AM</option>
-                <option value="05:00">5:00AM</option>
-                <option value="06:00">6:00AM</option>
-                <option value="07:00">7:00AM</option>
-                <option value="08:00">8:00AM</option>
-                <option value="09:00">9:00AM</option>
-                <option value="10:00">10:00AM</option>
-                <option value="11:00">11:00AM</option>
-                <option value="12:00">12:00PM</option>
-                <option value="13:00">1:00PM</option>
-                <option value="14:00">2:00PM</option>
-                <option value="15:00">3:00PM</option>
-                <option value="16:00">4:00PM</option>
-                <option value="17:00">5:00PM</option>
-                <option value="18:00">6:00PM</option>
-                <option value="19:00">7:00PM</option>
-                <option value="20:00">8:00PM</option>
-                <option value="21:00">9:00PM</option>
-                <option value="22:00">10:00PM</option>
-                <option value="23:00">11:00PM</option>
-            </select>
-            </a>
-        </div>
-
-        <div class="day-container">
-            <span class="label-day"></span>
-            <span class="day wed">31
-                <span class="name"><?php echo localize("vino.guide.day.wednesday.short") ?></span>
-            </span>
-
-            <a href="javascript:void(0)" navi_target navi_no_reset class="day-select">
-            <span>12:00AM</span>
-            <select name="day3" id="day3">
-                <option selected value="00:00">12:00AM</option>
-                <option value="01:00">1:00AM</option>
-                <option value="02:00">2:00AM</option>
-                <option value="03:00">3:00AM</option>
-                <option value="04:00">4:00AM</option>
-                <option value="05:00">5:00AM</option>
-                <option value="06:00">6:00AM</option>
-                <option value="07:00">7:00AM</option>
-                <option value="08:00">8:00AM</option>
-                <option value="09:00">9:00AM</option>
-                <option value="10:00">10:00AM</option>
-                <option value="11:00">11:00AM</option>
-                <option value="12:00">12:00PM</option>
-                <option value="13:00">1:00PM</option>
-                <option value="14:00">2:00PM</option>
-                <option value="15:00">3:00PM</option>
-                <option value="16:00">4:00PM</option>
-                <option value="17:00">5:00PM</option>
-                <option value="18:00">6:00PM</option>
-                <option value="19:00">7:00PM</option>
-                <option value="20:00">8:00PM</option>
-                <option value="21:00">9:00PM</option>
-                <option value="22:00">10:00PM</option>
-                <option value="23:00">11:00PM</option>
-            </select>
-            </a>
-        </div>
-
-        <div class="day-container">
-            <span class="label-day"></span>
-            <span class="day thu">1
-                <span class="name"><?php echo localize("vino.guide.day.thursday.short") ?></span>
-            </span>
-
-            <a href="javascript:void(0)" navi_target navi_no_reset class="day-select">
-            <span>12:00AM</span>
-            <select name="day4" id="day4">
-                <option selected value="00:00">12:00AM</option>
-                <option value="01:00">1:00AM</option>
-                <option value="02:00">2:00AM</option>
-                <option value="03:00">3:00AM</option>
-                <option value="04:00">4:00AM</option>
-                <option value="05:00">5:00AM</option>
-                <option value="06:00">6:00AM</option>
-                <option value="07:00">7:00AM</option>
-                <option value="08:00">8:00AM</option>
-                <option value="09:00">9:00AM</option>
-                <option value="10:00">10:00AM</option>
-                <option value="11:00">11:00AM</option>
-                <option value="12:00">12:00PM</option>
-                <option value="13:00">1:00PM</option>
-                <option value="14:00">2:00PM</option>
-                <option value="15:00">3:00PM</option>
-                <option value="16:00">4:00PM</option>
-                <option value="17:00">5:00PM</option>
-                <option value="18:00">6:00PM</option>
-                <option value="19:00">7:00PM</option>
-                <option value="20:00">8:00PM</option>
-                <option value="21:00">9:00PM</option>
-                <option value="22:00">10:00PM</option>
-                <option value="23:00">11:00PM</option>
-            </select>
-            </a>
-        </div>
-
-        <div class="day-container">
-            <span class="label-day"></span>
-            <span class="day fri">2
-                <span class="name"><?php echo localize("vino.guide.day.friday.short") ?></span>
-            </span>
-
-            <a href="javascript:void(0)" navi_target navi_no_reset class="day-select">
-            <span>12:00AM</span>
-            <select name="day5" id="day5">
-                <option selected value="00:00">12:00AM</option>
-                <option value="01:00">1:00AM</option>
-                <option value="02:00">2:00AM</option>
-                <option value="03:00">3:00AM</option>
-                <option value="04:00">4:00AM</option>
-                <option value="05:00">5:00AM</option>
-                <option value="06:00">6:00AM</option>
-                <option value="07:00">7:00AM</option>
-                <option value="08:00">8:00AM</option>
-                <option value="09:00">9:00AM</option>
-                <option value="10:00">10:00AM</option>
-                <option value="11:00">11:00AM</option>
-                <option value="12:00">12:00PM</option>
-                <option value="13:00">1:00PM</option>
-                <option value="14:00">2:00PM</option>
-                <option value="15:00">3:00PM</option>
-                <option value="16:00">4:00PM</option>
-                <option value="17:00">5:00PM</option>
-                <option value="18:00">6:00PM</option>
-                <option value="19:00">7:00PM</option>
-                <option value="20:00">8:00PM</option>
-                <option value="21:00">9:00PM</option>
-                <option value="22:00">10:00PM</option>
-                <option value="23:00">11:00PM</option>
-            </select>
-            </a>
-        </div>
-
-        <div class="day-container">
-            <span class="label-day"></span>
-            <span class="day sat">3
-                <span class="name"><?php echo localize("vino.guide.day.saturday.short") ?></span>
-            </span>
-
-            <a href="javascript:void(0)" navi_target navi_no_reset class="day-select">
-            <span>12:00AM</span>
-            <select name="day6" id="day6">
-                <option selected value="00:00">12:00AM</option>
-                <option value="01:00">1:00AM</option>
-                <option value="02:00">2:00AM</option>
-                <option value="03:00">3:00AM</option>
-                <option value="04:00">4:00AM</option>
-                <option value="05:00">5:00AM</option>
-                <option value="06:00">6:00AM</option>
-                <option value="07:00">7:00AM</option>
-                <option value="08:00">8:00AM</option>
-                <option value="09:00">9:00AM</option>
-                <option value="10:00">10:00AM</option>
-                <option value="11:00">11:00AM</option>
-                <option value="12:00">12:00PM</option>
-                <option value="13:00">1:00PM</option>
-                <option value="14:00">2:00PM</option>
-                <option value="15:00">3:00PM</option>
-                <option value="16:00">4:00PM</option>
-                <option value="17:00">5:00PM</option>
-                <option value="18:00">6:00PM</option>
-                <option value="19:00">7:00PM</option>
-                <option value="20:00">8:00PM</option>
-                <option value="21:00">9:00PM</option>
-                <option value="22:00">10:00PM</option>
-                <option value="23:00">11:00PM</option>
-            </select>
-            </a>
-        </div>
-
-        </div>
+    </div>
 
         <a href="javascript:void(0)" navi_target navi_mouse navi_margin="2" data-sound="SE_A_CLOSE_TOUCH_OFF" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" data-hover class="option-button cancel-guide-option accesskey-b"> Cancel</a>
         <a href="javascript:void(0)" navi_target navi_mouse navi_margin="2" data-sound="SE_HELP_TOUCH_OFF" data-hover-out-sound="SE_CMN_TOUCH_CANCEL" data-hover-sound="SE_CMN_TOUCH_ON" data-hover class="help-guide-button">Help</a>
