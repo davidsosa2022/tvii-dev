068de47c76109fc5fb0fedd2c4fb4ff21ab67061
davey stone but bfdi mouth <118952873+davidsosa2022@users.noreply.github.com>
2024-08-06 16:49:44 -0500

    345

--- us/js/vino_US.js
+++ us/js/vino_US.js
@@ -389,7 +389,7 @@ var tvii = tvii || {
             for (var i = 0; i < tvii.templates.templateList.length; i++) {
                 (function (temToLoad) {
                     var xhr = new XMLHttpRequest();
-                    xhr.open("GET", tvii.clientUrl + "./templates/" + temToLoad.template_file);
+                    xhr.open("GET", tvii.clientUrl + "/us/templates/" + temToLoad.template_file);
                     xhr.onreadystatechange = function () {
                         if (xhr.readyState == 4) {
                             if (xhr.status == 200) {
